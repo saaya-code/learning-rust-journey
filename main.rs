c046baee0af67bb5f148274387e4ad4e82fdb801
saaya-code <boualimohamed74@gmail.com>
2024-01-13 16:56:16 +0100

    defining a function

--- main.rs
+++ main.rs
@@ -1,7 +1,13 @@
 fn main() {
-    let mut x:i32 = 1;
-    x+= 2; 
+    // let mut x:i32 = 1;
+    // x+= 2; 
     
-    assert_eq!(x, 3);
-    println!("Success!");
+    // assert_eq!(x, 3);
+    // println!("Success!");
+    define_x();
+}
+
+fn define_x(){
+    let x: &str = "Hello world!";
+    println!("THIS IS {}",x);
 }
